740e8ee8e265a2a2aded61250040ba1ff99dc238
Jürgen Belz <juergen.belz@physik.uni-marburg.de>
2017-10-24 18:28:38 +0200

    added code to feature a button for resetting the soil moisture reference value as well as pseudo alternating current measurement of the soil. the rare counter is used to check for the wet state and is triggered not very often

--- RF_Soil_tiny85_transmitter/RF_Soil_tiny85_transmitter.ino
+++ RF_Soil_tiny85_transmitter/RF_Soil_tiny85_transmitter.ino
@@ -1,10 +1,12 @@
 #define RH_SPEED 2000 // RadioHead bitrate in bit/s
-#define TX_PIN 3
-#define RX_PIN 4
+#define TX_PIN 3 // PB3
+#define RX_PIN 4 // PB4 // not needed
 
 #define LED_TIME 50 // blink time for the LED
 #define TRANS_TIME 200 // blink time for the LED
-#define BUZZER_PIN 1
+#define BUZZER_PIN 1 // PB1
+#define SOIL_SENSOR_POL1 A2 //PB3
+#define SOIL_SENSOR_POL2 A3 //PB4
 
 #include <RH_ASK.h>
 #include <avr/wdt.h>
@@ -17,11 +19,15 @@
 #endif
 
 struct {
-  byte soil_value;
-  byte soil_concentration;
-  byte soil_state;
-  unsigned long counter;
+  byte soil_value = 255;
+  byte soil_concentration = 100;
+  byte soil_reference = 255;
+  boolean soil_wet = true;
+  unsigned long counter = 1;
 } soil_data;
+
+#define WATER_THRESHOLD 30 // 30%
+
 byte zize = sizeof(soil_data);
 byte tx_buf[sizeof(soil_data)] = {0};
 #define RH_BUF_LEN sizeof(soil_data)
@@ -31,61 +37,110 @@ RH_ASK driver(RH_SPEED, RX_PIN, TX_PIN);
 void setup() {
   pinMode(BUZZER_PIN, OUTPUT);
 
-  pinMode(TX_PIN, OUTPUT);
-  pinMode(RX_PIN, INPUT);
-  
-  driver.init();
-  soil_data.soil_concentration = 100;
-  soil_data.soil_state = 1;
-  soil_data.soil_value = 255;
-  soil_data.counter = 1;
+  //pinMode(TX_PIN, OUTPUT);
+  //pinMode(RX_PIN, INPUT);
 
+  driver.init();
+  setReference();
 }
 
-void loop() {
-  for (byte i=1;i<255;i++){
-  digitalWrite(BUZZER_PIN,HIGH);
-  _delay_us(500);
-   digitalWrite(BUZZER_PIN,LOW);
-   _delay_us(500);
+void setReference() {
+  soil_data.soil_reference = measureSoil();
+}
+
+void causeNoise() { // some random noise, not optimized for resonance frequency
+  for (byte i = 1; i < 127; i++) {
+    digitalWrite(BUZZER_PIN, HIGH);
+    _delay_us(500);
+    digitalWrite(BUZZER_PIN, LOW);
+    _delay_us(500);
   }
-  
+}
+void processSensor() {
+  soil_data.soil_value = measureSoil();
+  soil_data.soil_concentration = (byte)(soil_data.soil_value / soil_data.soil_reference);
+  soil_data.counter++;
+  if ( soil_data.soil_concentration <= WATER_THRESHOLD ) {
+    soil_data.soil_wet = false;
+  }
+  else {
+    soil_data.soil_wet = true;
+  }
+}
+void loop() {
+  static boolean CheckBeep = false;
+
+  processSensor();
+  if ( soil_data.counter %= 100 ) {
+    CheckBeep = true;
+  }
+
+
   memcpy(tx_buf, &soil_data, sizeof(soil_data) );
+  pinMode(TX_PIN, OUTPUT); // needed because TX_PIN is also SOIL_SENSOR_POL1
   if (driver.send((uint8_t *)tx_buf, zize)) {
     driver.waitPacketSent();
   }
-  soil_data.counter++;
-  _delay_ms(TRANS_TIME);
-  // sleep bit patterns:
-  //  1 second:  0b000110
-  //  2 seconds: 0b000111
-  //  4 seconds: 0b100000
-  //  8 seconds: 0b100001
- 
+
+  //_delay_ms(TRANS_TIME); // not needed?
+
   for (int i = 1; i <= 2; i++) {
+    // sleep bit patterns:
+    //  1 second:  0b000110
+    //  2 seconds: 0b000111
+    //  4 seconds: 0b100000
+    //  8 seconds: 0b100001
     myWatchdogEnable (0b000111);  // 8x2 second
   }
 
+  if ( soil_data.soil_wet != true ) {
+    if ( CheckBeep ) {
+      causeNoise();
+      CheckBeep = false;
+    }
+  }
 }
+  ISR(WDT_vect) {
+    wdt_disable();
+  }
 
-ISR(WDT_vect) {
-  wdt_disable();
-}
+  void myWatchdogEnable(const byte interval) {
+    attachInterrupt(0, setReference, HIGH); // use interrupt 0 (pin PB2) and run
+    noInterrupts();
+    wdt_reset();
 
-void myWatchdogEnable(const byte interval) {
-  noInterrupts();
+    MCUSR = 0;                          // reset various flags
+    WDTCR |= 0b00011000;               // see docs, set WDCE, WDE
+    WDTCR =  0b01000000 | interval;    // set WDIE, and appropriate delay
 
-  wdt_reset();
+    ADCSRA &= ~_BV(ADEN);
 
-  MCUSR = 0;                          // reset various flags
-  WDTCR |= 0b00011000;               // see docs, set WDCE, WDE
-  WDTCR =  0b01000000 | interval;    // set WDIE, and appropriate delay
+    set_sleep_mode (SLEEP_MODE_PWR_DOWN);
+    sleep_bod_disable();
+    interrupts();
+    sleep_mode();
 
-  ADCSRA &= ~_BV(ADEN);
+  }
 
-  set_sleep_mode (SLEEP_MODE_PWR_DOWN);
-  sleep_bod_disable();
-  interrupts();
-  sleep_mode();
-
-}
+  byte measureSoil() {
+    static byte numIter = 100;
+    unsigned short value = 0;
+    for (byte i = 1; i <= numIter; i++) {
+      //if ( soil_data.counter %= 2 ) {
+      if ( i %= 2 ) {
+        pinMode(SOIL_SENSOR_POL1, OUTPUT);
+        pinMode(SOIL_SENSOR_POL2, INPUT); // use pull/push resistors
+        digitalWrite( SOIL_SENSOR_POL1, HIGH );
+        value += analogRead( SOIL_SENSOR_POL2 );
+      }
+      else {
+        pinMode(SOIL_SENSOR_POL1, INPUT); // use pull/push resistors
+        pinMode(SOIL_SENSOR_POL2, OUTPUT);
+        digitalWrite( SOIL_SENSOR_POL2, HIGH );
+        value += analogRead( SOIL_SENSOR_POL1 );
+      }
+    }
+    value /= numIter;
+    value /= 4; // roughly remap 1024 DAC values to a 255 (byte)
+    return (byte)value;
+  }
