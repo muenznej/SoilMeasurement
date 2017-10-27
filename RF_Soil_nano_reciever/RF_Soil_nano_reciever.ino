b15e2ff2d6f8a24ec04b89eae54e55a1e71a6d8c
Juergen Belz <juergen.muenzner@web.de>
2017-10-27 18:46:30 +0200

    added flip flop voltage for soilmeasureing at nano, but there is smth wrong with AREF

--- RF_Soil_nano_reciever/RF_Soil_nano_reciever.ino
+++ RF_Soil_nano_reciever/RF_Soil_nano_reciever.ino
@@ -5,6 +5,10 @@
 #define TRANS_TIME 200 // blink time for the LED
 #define LED_RECIEVING 4
 
+#define SOIL_SENSOR_POL1 6
+#define SOIL_SENSOR_POL2 7
+#define SOIL_SENSOR_READ A5
+
 struct {
   byte soil_value = 255;
   byte soil_concentration = 100;
@@ -22,6 +26,12 @@ RH_ASK driver(2000, 11, 12);
 
 
 void setup() {
+  pinMode(SOIL_SENSOR_POL1, OUTPUT);
+  pinMode(SOIL_SENSOR_POL2, OUTPUT);
+  digitalWrite(SOIL_SENSOR_POL1, HIGH);
+  digitalWrite(SOIL_SENSOR_POL2, HIGH);
+  pinMode(SOIL_SENSOR_READ, INPUT);
+
   pinMode(LED_RECIEVING, OUTPUT);
   Serial.begin(9600);
   if (!driver.init()) {
@@ -30,9 +40,18 @@ void setup() {
   else {
     Serial.println("init successful");
   }
+  analogReference(EXTERNAL);
 }
 
 void loop() {
+  digitalWrite(SOIL_SENSOR_POL1, HIGH);
+  digitalWrite(SOIL_SENSOR_POL2, LOW);
+  Serial.print("Moisture: ");
+  //Serial.println(measureSoil(), DEC);
+  Serial.println( analogRead( SOIL_SENSOR_READ ), DEC);
+  Serial.print("");
+
+  _delay_ms(1000);
 
   digitalWrite(LED_RECIEVING, LOW);
   if (driver.recv(rx_buf, &zize)) {
@@ -59,3 +78,23 @@ void loop() {
   _delay_ms(TRANS_TIME);
 }
 
+float measureSoil() {
+  byte numIter = 10;
+  float value = 0;
+  for (byte i = 1; i <= numIter; i++) {
+    //   if ( i%2 ) {
+    pinMode(SOIL_SENSOR_POL2, OUTPUT);
+    digitalWrite(SOIL_SENSOR_POL2, HIGH);
+    _delay_ms(5);
+    value += analogRead( SOIL_SENSOR_READ );
+    //    }
+    //    else {
+    //      pinMode(SOIL_SENSOR_POL2, INPUT);
+    //      pinMode(SOIL_SENSOR_POL1, OUTPUT);
+    //      _delay_ms(5);
+    //      value += analogRead( SOIL_SENSOR_POL2 );
+  }
+  value /= numIter;
+  //value /= 4;
+  return value;
+}
